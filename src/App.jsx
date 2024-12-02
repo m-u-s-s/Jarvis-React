c7df089da900b2b5051931ab23cc606be80bb404
m-u-s-s <mm.darouich@hotmail.com>
2024-12-02 01:53:37 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../src/components/sidebar/sidebar";
+import Main from "../src/components/main/Main";
+
+const App = () => {
+  return (
+    <>
+        <Sidebar />
+        <Main />
+    </>
+  );
+};
+
+export default App;
